b2d51bb87423ed457f5a60e63943df25f51eefae
technologist25 <priteshvishwakarma@technologist.com>
2019-04-17 11:56:10 +0530

    optimization

--- app/src/main/java/com/example/dynamicviewpager/MainActivity.java
+++ app/src/main/java/com/example/dynamicviewpager/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Handler;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import java.util.ArrayList;
 import java.util.Timer;
@@ -13,24 +12,21 @@ import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ViewPager pager = null;
-    private MainPagerAdapter pagerAdapter = null;
-    ArrayList<String> list = new ArrayList<>();
-    private int currentPage = 0;
-
-    /**
-     * https://homepages.cae.wisc.edu/~ece533/images/airplane.png
-     * https://homepages.cae.wisc.edu/~ece533/images/arctichare.png
-     * https://homepages.cae.wisc.edu/~ece533/images/baboon.png
-     * https://homepages.cae.wisc.edu/~ece533/images/barbara.bmp
-     * https://homepages.cae.wisc.edu/~ece533/images/boat.png
-     */
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        productAdvertise();
+        setTimer();
+    }
+
+    private void productAdvertise() {
+
+        //ViewPager pager = null;
+        //MainPagerAdapter pagerAdapter = null;
+        final int[] currentPage = {0};
+        final ArrayList<String> list = new ArrayList<>();
 
         list.add("https://homepages.cae.wisc.edu/~ece533/images/airplane.png");
         list.add("https://homepages.cae.wisc.edu/~ece533/images/arctichare.png");
@@ -38,8 +34,8 @@ public class MainActivity extends AppCompatActivity {
         list.add("https://homepages.cae.wisc.edu/~ece533/images/barbara.bmp");
         list.add("https://homepages.cae.wisc.edu/~ece533/images/boat.png");
 
-        pagerAdapter = new MainPagerAdapter(getApplicationContext(), list);
-        pager = findViewById(R.id.view_pager);
+        MainPagerAdapter pagerAdapter = new MainPagerAdapter(getApplicationContext(), list);
+        final ViewPager pager = findViewById(R.id.view_pager);
         pager.setAdapter(pagerAdapter);
 
         for (int i = 0; i < list.size(); i++) {
@@ -51,18 +47,13 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = findViewById(R.id.tabDots);
         tabLayout.setupWithViewPager(pager, true);
 
-        setTimer();
-    }
-
-    private void setTimer() {
-
         final Handler handler = new Handler();
         final Runnable Update = new Runnable() {
             public void run() {
-                if (currentPage == list.size()) {
-                    currentPage = 0;
+                if (currentPage[0] == list.size()) {
+                    currentPage[0] = 0;
                 }
-                pager.setCurrentItem(currentPage++, true);
+                pager.setCurrentItem(currentPage[0]++, true);
             }
         };
         Timer timer = new Timer(); // This will create a new Thread
@@ -74,26 +65,9 @@ public class MainActivity extends AppCompatActivity {
         }, 500, 1800);
     }
 
-    public void addView(View newPage) {
-        int pageIndex = pagerAdapter.addView(newPage);
-        // You might want to make "newPage" the currently displayed page:
-        pager.setCurrentItem(pageIndex, true);
-    }
+    private void setTimer() {
 
-    public void removeView(View defunctPage) {
-        int pageIndex = pagerAdapter.removeView(pager, defunctPage);
-        // You might want to choose what page to display, if the current page was "defunctPage".
-        if (pageIndex == pagerAdapter.getCount())
-            pageIndex--;
-        pager.setCurrentItem(pageIndex);
-    }
 
-    public View getCurrentPage() {
-        return pagerAdapter.getView(pager.getCurrentItem());
-    }
-
-    public void setCurrentPage(View pageToShow) {
-        pager.setCurrentItem(pagerAdapter.getItemPosition(pageToShow), true);
     }
 
 }
